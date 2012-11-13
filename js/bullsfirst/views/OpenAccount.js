7a1f44b140d46ea76d5529825868ce0f457b4f14
rajakvk <rajakvk@gmail.com>
2012-11-13 18:20:39 +0530

    Integrating with backend to open account
    Backend throwing 500 internal error. Checking in to share the code to
    discuss further with Naresh

--- js/bullsfirst/views/OpenAccount.js
+++ js/bullsfirst/views/OpenAccount.js
@@ -20,11 +20,24 @@
  * @author Kanakaraj Venkataswamy
  */
 define(['bullsfirst/domain/Credentials',
+        'bullsfirst/domain/ExternalAccount',
+        'bullsfirst/domain/ExternalAccounts',
+		'bullsfirst/domain/User',
+		'bullsfirst/domain/UserContext',
+        'bullsfirst/framework/MessageBus',
         'bullsfirst/framework/Page',
-        'bullsfirst/services/UserService'],
-       function(Credentials, Page, UserService) {
+		'bullsfirst/framework/errorUtil',
+        'bullsfirst/services/UserService',
+        'bullsfirst/services/AccountService',
+        'bullsfirst/services/BrokerageAccountService'],
+       function(Credentials, ExternalAccount, ExternalAccounts, User, UserContext, MessageBus, Page, ErrorUtil, UserService, AccountService, BrokerageAccountService) {
     return Page.extend({
-        el: '#open-account-page',
+		
+		brokerageAccountId: 0,
+		 
+        externalAccountId : 0,
+        
+		el: '#open-account-page',
 
         events: {
 			'click #open-account-button': 'handleOpenAccountButton',
@@ -37,13 +50,85 @@ define(['bullsfirst/domain/Credentials',
 
         handleOpenAccountButton: function(event) {
 			if ($('#open-account-form').validationEngine('validate')) {
-				alert('valid');
+				this.createUser();
             }
             return false;
         },
 		
+		createUserDone: function(data, textStatus, jqXHR){
+			// Add user to UserContext
+			UserContext.initUser(this.form2User());
+			UserContext.initCredentials(this.form2Credentials());
+			
+			// Create brokerage account
+			BrokerageAccountService.createBrokerageAccount(
+				'Brokerage Account 1', _.bind(this.createBrokerageAccountDone, this), ErrorUtil.showError);
+		},
+		
+		createBrokerageAccountDone: function(data, textStatus, jqXHR){
+			this.brokerageAccountId = data.id;
+			
+			// Create external account
+			var externalAccount = new ExternalAccount({
+				name: 'External Account 1', routingNumber: '022000248', accountNumber: '12345678'
+			});
+			UserContext.getExternalAccounts().add(externalAccount);
+			externalAccount.save(null, {
+				success: _.bind(this.createExternalAccountDone, this),
+				error: ErrorUtil.showBackboneError
+			});
+		},
+		
+		createExternalAccountDone: function(model, jqXHR){
+			this.externalAccountId = model.id;
+			
+			// Transfer cash
+			AccountService.transferCash(
+				this.externalAccountId,
+				{ amount: {amount: 100000, currency: 'USD'}, toAccountId: this.brokerageAccountId },
+				_.bind(this.transferCashDone, this),
+				ErrorUtil.showError
+			);
+		},
+		
+		transferCashDone: function(data, textStatus, jqXHR){
+			// TODO: Erase the form
+			MessageBus.trigger('UserLoggedInEvent');
+		},
+		
 		handleCloseButton: function(){
 			$('#open-account-form').validationEngine('hide');
+		},
+		
+		createUser: function(){
+			UserService.createUser(
+					this.form2CreateUserRequest(), _.bind(this.createUserDone, this), ErrorUtil.showError);
+		},
+		
+		// ------------------------------------------------------------
+        // Helper functions
+        // ------------------------------------------------------------
+        // Creates a CreateUserRequest from the Open Account form
+		form2CreateUserRequest: function(){
+			var formObject = $('#open-account-form').toObject();
+            delete formObject['confirmPassword']; // property not expected by REST service
+            return formObject;
+		},
+		
+		// Creates a User from the Open Account form
+		form2User: function(){
+			var formObject = $('#open-account-form').toObject();
+			delete formObject.password;
+			delete formObject['confirmPassword'];
+			return formObject;
+		},
+		
+		// Creates Credentials from the Open Account form
+		form2Credentials: function(){
+			return new Credentials(
+				$('#oa-username').val(),
+				$('#oa-password').val()
+			);
 		}
 		
     });
